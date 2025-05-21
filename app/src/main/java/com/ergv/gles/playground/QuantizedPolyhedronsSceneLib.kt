1a2cc525cbd022a885f2997b8b407ef5c695d660
er-gv <erezg3@gmail.com>
2025-05-21 18:56:26 +0300

    completed adding element buffer object support to all three scenes

--- app/src/main/java/com/ergv/gles/playground/QuantizedPolyhedronsSceneLib.kt
+++ app/src/main/java/com/ergv/gles/playground/QuantizedPolyhedronsSceneLib.kt
@@ -12,6 +12,7 @@ class QuantizedPolyhedronsSceneLib: AbstractJNILib() {
 
     external fun nativeInit(width: Int, height: Int)
     external fun nativeStep()
+    external fun nativeDestroy()
 
     public override fun init(width: Int, height: Int){
         nativeInit(width, height)
@@ -19,6 +20,10 @@ class QuantizedPolyhedronsSceneLib: AbstractJNILib() {
 
     public override fun step(){
         nativeStep()
+    }
+
+    public override fun destroy(){
+        nativeDestroy();
     }
 
 
